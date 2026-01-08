69b02bf5c7f7c8fb050674efe52de209ee9cae69
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2026-01-08 09:15:04 -0300

    feat: update projects

--- app/components/projects-section.tsx
+++ app/components/projects-section.tsx
@@ -7,6 +7,15 @@ export function ProjectsSection() {
       <h2 className="text-[15px] font-[450] tracking-[-0.16px] text-neutral-900 dark:text-neutral-100">Projects</h2>
       <div className="mt-6 flex flex-col gap-3">
         <ProjectItem
+          image="/projects/turbosql.png"
+          title="TurboSQL"
+          description="The fastest SQL GUI you've ever used"
+          projectUrl="https://turbosql.co"
+          date="May 2025"
+          imageWidth={28}
+          imageHeight={28}
+        />
+        <ProjectItem
           image="/projects/minilist.svg"
           title="Mini List"
           description="Minimalist to-do list app"
@@ -21,17 +30,8 @@ export function ProjectsSection() {
           description="The ultimate OG image gallery"
           projectUrl="https://ogpreview.co"
           date="Oct 2025"
-          icon={<EyeIcon className="size-7 text-sky-600 dark:text-sky-400" />}
-        />
-        <ProjectItem
-          image="/projects/turbosql.png"
-          title="TurboSQL"
-          description="The fastest SQL GUI you've ever used"
-          projectUrl="https://turbosql.co"
-          date="May 2025"
           tag="Archived"
-          imageWidth={28}
-          imageHeight={28}
+          icon={<EyeIcon className="size-7 text-sky-600 dark:text-sky-400" />}
         />
         <ProjectItem
           image="/projects/banana-post.svg"
